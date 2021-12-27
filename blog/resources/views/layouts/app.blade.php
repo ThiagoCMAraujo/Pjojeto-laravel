41084bfd8ca82c00906af0cc7be2645d9087e58f
tmelo387 <tmelo387@gmail.com>
2021-12-27 18:53:03 -0300

    att styles

---
+++ blog/resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="bg-gray-900">
+    <nav class="p-6 bg-white flex justify-between mb-5">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Thiago Melo</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+
+    </nav>
+    @yield('content')
+</body>
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+</html>
---
+++ blog/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+    ],
+    theme: {
+      extend: {
+          colors: {
+            'midnight': '#121063',
+          },
+      },
+    },
+    plugins: [],
+  }
